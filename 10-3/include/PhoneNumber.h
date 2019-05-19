9ff431f6254d37e965da3cc072a3eb18ded77af0
wang_xiaoyuyuyuyu <1981109481@qq.com>
2019-05-19 18:24:40 +0800

    第十章练习，测绘工程4班王钰。

---
+++ 10-3/include/PhoneNumber.h
@@ -0,0 +1,15 @@
+#ifndef PHONENUMBER_H
+#define PHONENUMBER_H
+
+
+class PhoneNumber
+{
+   friend std::ostream &operator<<( std::ostream &, const PhoneNumber & );
+   friend std::istream &operator>>( std::istream &, PhoneNumber & );
+private:
+    std::string areaCode;
+    std::string exchange;
+    std::string line;
+};
+
+#endif
